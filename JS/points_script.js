5d447f8d7bd7542d9db618beb62f348af98cc1e7
ekyahouses <139715994+ekyahouses@users.noreply.github.com>
2023-07-16 18:30:33 -0400

    Add files via upload

--- JS/points_script.js
+++ JS/points_script.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const scoreboardContainer = document.querySelector('.scoreboard-container');
     const houseElements = scoreboardContainer.querySelectorAll('.house');
 
-
-    console.log(houseElements[0]);
+    const modal = document.getElementById('myModal');
+    const modalHouseName = document.getElementById('modalHouseName');
+    const modalEventsTable = document.getElementById('modalEventsTable').getElementsByTagName('tbody')[0];
 
     // Replace 'points.csv' with the relative path to your CSV file on GitHub Pages
     const csvUrl = 'Data/points.csv';
@@ -75,4 +76,41 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+
+    houseElements.forEach(houseElement => {
+        houseElement.addEventListener('click', () => {
+            const houseName = houseElement.getAttribute('data-house');
+            const houseEvents = getEventsByHouse(houseName, pointsData);
+            showEventsPopup(houseName, houseEvents);
+        });
+    });
+
+    function showEventsPopup(houseName, houseEvents) {
+        modalHouseName.textContent = houseName;
+        modalEventsTable.innerHTML = '';
+
+        houseEvents.forEach(eventData => {
+            const { event, date, points } = eventData;
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${event}</td>
+                <td>${date}</td>
+                <td>${points}</td>
+            `;
+            modalEventsTable.appendChild(row);
+        });
+
+        modal.style.display = 'block';
+    }
+
+    // Close the modal when the user clicks on the 'x' button or outside the modal
+    const modalCloseBtn = document.querySelector('.close');
+    window.addEventListener('click', event => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+    modalCloseBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
 });
