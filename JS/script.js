9927d026863144fa113a817fddc4c196feec4475
EkyaHouses <139715994+EkyaHouses@users.noreply.github.com>
2023-07-16 16:20:39 -0400

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,44 @@
+// JavaScript to fetch and populate the events from the CSV file
+window.addEventListener('DOMContentLoaded', () => {
+    const eventsTableBody = document.querySelector('#events-table tbody');
+
+    // Replace 'events.csv' with the actual path to your CSV file
+    fetch("events.csv")
+        .then(response => response.text())
+        .then(data => {
+            const events = parseCSV(data);
+            populateEventsTable(events);
+        })
+        .catch(error => console.error('Error fetching events:', error));
+
+    function parseCSV(data) {
+        const rows = data.split('\n');
+        const events = [];
+
+        for (let i = 1; i < rows.length; i++) {
+            const columns = rows[i].split(',');
+            const eventName = columns[0].trim();
+            const date = columns[1].trim();
+            const linkOrContact = columns[2].trim();
+            events.push({ eventName, date, linkOrContact });
+        }
+
+        return events;
+    }
+
+    function populateEventsTable(events) {
+        let tableHTML = '';
+
+        events.forEach(event => {
+            tableHTML += `
+                <tr>
+                    <td>${event.eventName}</td>
+                    <td>${event.date}</td>
+                    <td>${event.linkOrContact}</td>
+                </tr>
+            `;
+        });
+
+        eventsTableBody.innerHTML = tableHTML;
+    }
+});
